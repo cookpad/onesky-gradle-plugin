b817ebeb70136ba5daa71fd0bc41adb29acbbbb4
Kentaro Takiguchi <takiguchi0817@gmail.com>
2017-05-26 17:01:00 +0100

    Validate locales param

--- plugin/src/main/java/rejasupotaro/onesky/plugin/DownloadTranslationTask.kt
+++ plugin/src/main/java/rejasupotaro/onesky/plugin/DownloadTranslationTask.kt
@@ -5,8 +5,19 @@ import org.gradle.api.tasks.TaskAction
 import java.io.File
 
 open class DownloadTranslationTask : OneskyTask() {
-    val resDir by lazy {
-        File("${project.projectDir.absolutePath}/src/main/res")
+    val resDir by lazy { File("${project.projectDir.absolutePath}/src/main/res") }
+
+    val locales by lazy {
+        if (oneskyExtension.locales.isNotEmpty()) {
+            oneskyExtension.locales.split(" ")
+        } else {
+            resDir
+                    .listFiles()
+                    .filter { it.name.startsWith("values-") }
+                    .map { it.name.replace("values-", "") }
+                    .map { it.replace("in", "id") }
+                    .map { it.replace("-r", "-") }
+        }
     }
 
     init {
@@ -16,7 +27,11 @@ open class DownloadTranslationTask : OneskyTask() {
 
     @TaskAction
     fun downloadTranslation() {
-        locales().forEach { locale ->
+        if (locales.isEmpty()) {
+            throw IllegalArgumentException("You should provide locales or values-* directories should be created before running this task")
+        }
+
+        locales.forEach { locale ->
             val (request, response, result) = oneskyClient.download(locale)
             when (result) {
                 is Result.Success -> {
@@ -27,19 +42,6 @@ open class DownloadTranslationTask : OneskyTask() {
                     throw result.error
                 }
             }
-        }
-    }
-
-    private fun locales(): List<String> {
-        if (oneskyExtension.locales.isNotEmpty()) {
-            return oneskyExtension.locales.split(" ")
-        } else {
-            return resDir
-                    .listFiles()
-                    .filter { it.name.startsWith("values-") }
-                    .map { it.name.replace("values-", "") }
-                    .map { it.replace("in", "id") }
-                    .map { it.replace("-r", "-") }
         }
     }
 }
