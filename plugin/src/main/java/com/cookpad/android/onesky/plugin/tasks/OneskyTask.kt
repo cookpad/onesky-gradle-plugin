87d0a47aa11b96493a63df4df1ed27ec9dcccae7
Victor Albertos Gil <me@victoralbertos.com>
2021-07-30 13:54:47 +0200

    support gradle v7

--- plugin/src/main/java/com/cookpad/android/onesky/plugin/tasks/OneskyTask.kt
+++ plugin/src/main/java/com/cookpad/android/onesky/plugin/tasks/OneskyTask.kt
@@ -1,15 +1,15 @@
 package com.cookpad.android.onesky.plugin.tasks
 
-import org.gradle.api.DefaultTask
 import com.cookpad.android.onesky.plugin.OneskyExtension
 import com.cookpad.android.onesky.plugin.client.Onesky
+import org.gradle.api.DefaultTask
 
 open class OneskyTask : DefaultTask() {
-    val oneskyExtension by lazy {
+    internal val oneskyExtension by lazy {
         project.extensions.findByType(OneskyExtension::class.java)
     }
 
-    val oneskyClient by lazy {
+    internal val oneskyClient by lazy {
         val apiKey = oneskyExtension!!.apiKey
         val apiSecret = oneskyExtension!!.apiSecret
         val projectId = oneskyExtension!!.projectId
